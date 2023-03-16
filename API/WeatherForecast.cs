ed6db99052878cb19dc808103b32c7e19fe45917
SriLakshmi Vundavalli <VSriLakshmi@outlook.com>
2023-03-15 22:10:49 -0400

    End of Section2

---
+++ API/WeatherForecast.cs
@@ -0,0 +1,17 @@
+using System.Net.Http.Headers;
+using API.Controllers;
+
+namespace API;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string Summary { get; set; }
+
+   
+}
