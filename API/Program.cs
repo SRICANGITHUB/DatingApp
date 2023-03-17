f91efcb711a6919bbfe955e547c0fd6abcb21b8c
SriLakshmi Vundavalli <VSriLakshmi@outlook.com>
2023-03-16 22:53:11 -0400

    Added CORS

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 var app = builder.Build();
 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
